18a8251dbae39c5aa5cc599e7be1b6ed9453df6a
npav2 <npav2@DESKTOP-0M1I02N>
2024-06-06 13:58:46 +0500

    Добавьте файлы проекта.

---
+++ Переверни фишки/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿
+namespace Переверни_фишки
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.rules_Button = new System.Windows.Forms.Button();
+            this.start_game_button = new System.Windows.Forms.Button();
+            this.reset_Button = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.Location = new System.Drawing.Point(29, 13);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(444, 421);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.UseWaitCursor = true;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Красный",
+            "Оранджевый",
+            "Жёлтый",
+            "Зелёный",
+            "Голубой",
+            "Синий",
+            "Фиолетовый",
+            "Чёрный"});
+            this.comboBox1.Location = new System.Drawing.Point(727, 52);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(138, 28);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.UseWaitCursor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(507, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(216, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Цвет первой стороны фишки:";
+            this.label3.UseWaitCursor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label4.Location = new System.Drawing.Point(507, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(214, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Цвет второй стороны фишки:";
+            this.label4.UseWaitCursor = true;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.Color.White;
+            this.comboBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Красный",
+            "Оранджевый",
+            "Жёлтый",
+            "Зелёный",
+            "Голубой",
+            "Синий",
+            "Фиолетовый",
+            "Чёрный"});
+            this.comboBox2.Location = new System.Drawing.Point(727, 103);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(138, 28);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.UseWaitCursor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label5.Location = new System.Drawing.Point(507, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Время игры:";
+            this.label5.UseWaitCursor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label6.Location = new System.Drawing.Point(505, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(138, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Количество ходов:";
+            this.label6.UseWaitCursor = true;
+            // 
+            // rules_Button
+            // 
+            this.rules_Button.BackColor = System.Drawing.Color.White;
+            this.rules_Button.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.rules_Button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.rules_Button.Location = new System.Drawing.Point(497, 253);
+            this.rules_Button.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rules_Button.Name = "rules_Button";
+            this.rules_Button.Size = new System.Drawing.Size(122, 43);
+            this.rules_Button.TabIndex = 11;
+            this.rules_Button.Text = "Правила игры";
+            this.rules_Button.UseVisualStyleBackColor = false;
+            this.rules_Button.UseWaitCursor = true;
+            this.rules_Button.Click += new System.EventHandler(this.rulesButton_Click);
+            // 
+            // start_game_button
+            // 
+            this.start_game_button.BackColor = System.Drawing.Color.White;
+            this.start_game_button.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.start_game_button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.start_game_button.Location = new System.Drawing.Point(640, 253);
+            this.start_game_button.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.start_game_button.Name = "start_game_button";
+            this.start_game_button.Size = new System.Drawing.Size(114, 43);
+            this.start_game_button.TabIndex = 12;
+            this.start_game_button.Text = "Начать игру";
+            this.start_game_button.UseVisualStyleBackColor = false;
+            this.start_game_button.UseWaitCursor = true;
+            this.start_game_button.Click += new System.EventHandler(this.start_game_button_Click);
+            // 
+            // reset_Button
+            // 
+            this.reset_Button.BackColor = System.Drawing.Color.White;
+            this.reset_Button.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.reset_Button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.reset_Button.Location = new System.Drawing.Point(775, 253);
+            this.reset_Button.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.reset_Button.Name = "reset_Button";
+            this.reset_Button.Size = new System.Drawing.Size(114, 43);
+            this.reset_Button.TabIndex = 13;
+            this.reset_Button.Text = "Сброс";
+            this.reset_Button.UseVisualStyleBackColor = false;
+            this.reset_Button.UseWaitCursor = true;
+            this.reset_Button.Click += new System.EventHandler(this.reset_Button_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 900;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(609, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 20);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "0 : 0";
+            this.label1.UseWaitCursor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.textBox1.Location = new System.Drawing.Point(650, 199);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 27);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(928, 448);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.reset_Button);
+            this.Controls.Add(this.start_game_button);
+            this.Controls.Add(this.rules_Button);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Игра \"Переверни фишки\"";
+            this.UseWaitCursor = true;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button rules_Button;
+        private System.Windows.Forms.Button start_game_button;
+        private System.Windows.Forms.Button reset_Button;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Переверни фишки/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Переверни_фишки
+{
+
+    public partial class Form1 : Form
+    {
+        int moves; //переменная, хранящая количество ходов
+        int timeSecond = 0; //переменная, хранящая количество секунд 
+        int timeMinute = 0; //переменная, хранящая количество минут
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void rulesButton_Click(object sender, EventArgs e) //метод вызова окна с правилами игры при нажатии на кнопку
+        {
+            MessageBox.Show("Игровое поле представляет собой таблицу размером 4х4 клеток. В клетках размещены фишки двух выбранных цветов. При щелчке мышкой по какой - нибудь фишки, её цвет и цвет соседних по горизонтали и соседних по вертикали фишек изменяется на противоположный. После щелчка в центре игрового поля цвет изменят пять фишек, на краю поля — четыре, а в углу — только три фишки. Цель игры состоит в том, чтобы сделать все фишки одного и того же цвета.", "Правила игры", MessageBoxButtons.OK);
+        }
+        private void start_game_button_Click(object sender, EventArgs e) // метод для старта игры при нажатии на кнопку
+        {
+            if (comboBox1.Text == "" || comboBox2.Text == "") { MessageBox.Show("Выберите цвета сторон фишек!", "Ошибка"); timer1.Enabled = false; }
+            else if (comboBox1.Text == comboBox2.Text && comboBox1.Text != "") { MessageBox.Show("Цвета сторон фишек не должны совпадать!", "Ошибка"); timer1.Enabled = false; }
+            else
+            {
+                generationGameTable();
+                if (CheckWin() && moves == 0) // проверка на совпадение всех цветов при генерации игрового поля
+                {
+                    ClearDataGridView();
+                    generationGameTable();
+                }
+                else
+                {
+                    timer1.Enabled = true;
+                    dataGridView1.ClearSelection();
+                    comboBox1.Enabled = false;
+                    comboBox2.Enabled = false;
+                    start_game_button.Enabled = false;
+                }
+            }
+        }
+        private void reset_Button_Click(object sender, EventArgs e) //метод сброса состояния игры при нажатии на кнопку
+        {
+            DialogResult result = MessageBox.Show("Сбросить состояние игры?", "Сброс", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes) resetAll();
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e) //метод для изменения цвета выбранной и соседним ей фишек и прибавления единицы к количеству ходов;
+        {
+            dataGridView1.ClearSelection();
+            FlipCellColor(e.RowIndex, e.ColumnIndex);
+            dataGridView1.ClearSelection();
+            moves++;
+            textBox1.Text = moves.ToString();
+            if (CheckWin())
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("Вы выиграли!\nКоличество ходов: " + moves + "\nВремя прохождения игры: " + label1.Text, "Победа!");
+                resetAll();
+            }
+        }
+        private void generationGameTable() //метод заполнения игрового поля фишками выбранных цветов
+        {
+            Color color1, color2;
+            color1 = GetColor1();
+            color2 = GetColor2();
+            dataGridView1.RowCount = 4; //количество строк
+            dataGridView1.ColumnCount = 4; //количество столбцов
+            dataGridView1.ReadOnly = true;
+            int[,] a = new int[4, 4]; //инициализация матрицы
+            int i, j;
+            //заполнение игрового поля фишками и вывод
+            Random rand = new Random();
+            for (i = 0; i < dataGridView1.RowCount; i++)
+                for (j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    dataGridView1.Columns[i].Width = 100;
+                    dataGridView1.Rows[i].Height = 100;
+                    a[i, j] = rand.Next(0, 2);
+                    if (a[i, j] == 0)
+                        dataGridView1.Rows[i].Cells[j].Style.BackColor = color1;
+                    else dataGridView1.Rows[i].Cells[j].Style.BackColor = color2;
+                    dataGridView1.Rows[i].Cells[j].Value = "";
+                }
+        }
+        private void FlipCellColor(int row, int col) //метод проверки положения выбранной фишки и изменения её цвета;
+        {
+            Color color1, color2;
+            color1 = GetColor1();
+            color2 = GetColor2();
+            if (dataGridView1.Rows[row].Cells[col].Style.BackColor == color1)
+                dataGridView1.Rows[row].Cells[col].Style.BackColor = color2;
+            else dataGridView1.Rows[row].Cells[col].Style.BackColor = color1;
+            // Проверка и переключение цветов соседних ячеек
+            if (row > 0) // Проверка верхней ячейки
+                if (dataGridView1.Rows[row - 1].Cells[col].Style.BackColor == color1)
+                    dataGridView1.Rows[row - 1].Cells[col].Style.BackColor = color2;
+                else dataGridView1.Rows[row - 1].Cells[col].Style.BackColor = color1;
+            if (row < 3) // Проверка нижней ячейки
+                if (dataGridView1.Rows[row + 1].Cells[col].Style.BackColor == color1)
+                    dataGridView1.Rows[row + 1].Cells[col].Style.BackColor = color2;
+                else dataGridView1.Rows[row + 1].Cells[col].Style.BackColor = color1;
+            if (col > 0) // Проверка левой ячейки
+                if (dataGridView1.Rows[row].Cells[col - 1].Style.BackColor == color1)
+                    dataGridView1.Rows[row].Cells[col - 1].Style.BackColor = color2;
+                else dataGridView1.Rows[row].Cells[col - 1].Style.BackColor = color1;
+            if (col < 3) // Проверка правой ячейки
+                if (dataGridView1.Rows[row].Cells[col + 1].Style.BackColor == color1)
+                    dataGridView1.Rows[row].Cells[col + 1].Style.BackColor = color2;
+                else dataGridView1.Rows[row].Cells[col + 1].Style.BackColor = color1;
+        }
+
+        private bool CheckWin() //метод проверки цветов фишек на игровом поле
+        {
+            bool allColor1 = true;
+            bool allColor2 = true;
+            Color color1, color2;
+            color1 = GetColor1();
+            color2 = GetColor2();
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                {
+                    if (dataGridView1.Rows[i].Cells[j].Style.BackColor != color1) //проверка на несовпадение первого выбранного цвета и цветов всех фишек
+                        allColor1 = false;
+                    if (dataGridView1.Rows[i].Cells[j].Style.BackColor != color2) //проверка на несовпадение первого выбранного цвета и цветов всех фишек
+                        allColor2 = false;
+                }
+            if (allColor1 || allColor2)
+                return true;
+            else return false;
+        }
+        private void ClearDataGridView() //метод очистки игрового поля;
+        {
+            dataGridView1.Rows.Clear(); // Очищаем строки
+            dataGridView1.Columns.Clear(); // Очищаем столбцы
+        }
+        private void resetAll() //метод сброса состояния игры
+        {
+            comboBox1.Text = "";
+            comboBox2.Text = "";
+            comboBox1.Enabled = true;
+            comboBox2.Enabled = true;
+            timer1.Enabled = false;
+            label1.Text = "0 : 0";
+            textBox1.Text = "";
+            timeSecond = 0;
+            timeMinute = 0;
+            ClearDataGridView();
+            moves = 0;
+            start_game_button.Enabled = true;
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1_CellClick(sender, e);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) //метод для обработки события «Tick» таймера
+        {
+            timeSecond++;
+            if (timeSecond == 60) { timeSecond = 0; timeMinute++; }
+            label1.Text = timeMinute.ToString() + " : " + timeSecond.ToString();
+        }
+        private Color GetColor1()
+        {
+            switch (comboBox1.SelectedItem.ToString())
+            {
+                case "Красный":
+                    return Color.Red;
+                case "Оранджевый":
+                    return Color.Orange;
+                case "Жёлтый":
+                    return Color.Yellow;
+                case "Зелёный":
+                    return Color.Green;
+                case "Синий":
+                    return Color.Blue;
+                case "Фиолетовый":
+                    return Color.Purple;
+                case "Чёрный":
+                    return Color.Black;
+                default:
+                    return Color.White; 
+            }
+        }
+
+        private Color GetColor2()
+        {
+            switch (comboBox2.SelectedItem.ToString())
+            {
+                case "Красный":
+                    return Color.Red;
+                case "Оранджевый":
+                    return Color.Orange;
+                case "Жёлтый":
+                    return Color.Yellow;
+                case "Зелёный":
+                    return Color.Green;
+                case "Синий":
+                    return Color.Blue;
+                case "Фиолетовый":
+                    return Color.Purple;
+                case "Чёрный":
+                    return Color.Black;
+                default:
+                    return Color.White; 
+            }
+        }
+    }
+}
